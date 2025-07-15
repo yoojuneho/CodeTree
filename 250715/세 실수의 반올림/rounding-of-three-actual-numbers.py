9d4b8e54310774dcc4561e2b1d5550802ebe749b
Junho Yoo <105614624+yoojuneho@users.noreply.github.com>
2025-07-15 15:51:37 +0900

    Title: 세 실수의 반올림, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250715/세 실수의 반올림/rounding-of-three-actual-numbers.py
@@ -0,0 +1,16 @@
+a = float(input())
+
+while a <= 1 and a >= 1000:
+    a = float(input())
+
+b = float(input())
+
+while b <= 1 and b >= 1000:
+    b = float(input())
+
+c = float(input())
+
+while c <= 1 and c >= 1000:
+    c = float(input())
+
+print(f"{a:.3f}\n{b:.3f}\n{c:.3f}")
