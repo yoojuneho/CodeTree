b4113fc65e275ff2c593f3ba02bea78e0d4ed581
Junho Yoo <105614624+yoojuneho@users.noreply.github.com>
2025-07-18 21:28:56 +0900

    Title: 일의 자리 배열, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250718/일의 자리 배열/array-with-units.py
+++ 250718/일의 자리 배열/array-with-units.py
@@ -1,7 +1,7 @@
 #a,b = map(int, input().split())
-arr = [map(int, input().split())]
+arr = list(map(int, input().split()))
 
-for i in range(2,11):
+for i in range(2,10):
     arr.append((arr[i-2]+arr[i-1])%10)
 
 for j in arr:
