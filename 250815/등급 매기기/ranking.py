3a76c8465b1d71202dfb0d9e683276165b9c5e3a
Junho Yoo <105614624+yoojuneho@users.noreply.github.com>
2025-08-15 22:00:37 +0900

    Title: 등급 매기기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250815/등급 매기기/ranking.py
@@ -0,0 +1,12 @@
+x = int(input())
+
+if x >= 90:
+    print('A')
+elif 80 <= x < 90:
+    print('B')
+elif 70 <= x < 80:
+    print('C')
+elif 60 <= x < 70:
+    print('D')
+elif x < 60:
+    print('F')
