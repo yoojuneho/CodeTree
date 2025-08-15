7cee3284fe86c990c53115e253723297f6ede601
Junho Yoo <105614624+yoojuneho@users.noreply.github.com>
2025-08-15 22:02:42 +0900

    Title: 알파벳에 따른 평가, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250815/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+N = input()
+
+if N == 'S':
+    print('Superior')
+elif N == 'A':
+    print('Excellent')
+elif N == 'B':
+    print('Good')
+elif N == 'C':
+    print('Usually')
+elif N == 'D':
+    print('Effort')
+else:
+    print('Failure')
