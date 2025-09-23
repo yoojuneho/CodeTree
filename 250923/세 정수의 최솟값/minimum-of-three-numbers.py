3a072c48072547ff43eb09ecc68ea110b6fe0e65
Junho Yoo <105614624+yoojuneho@users.noreply.github.com>
2025-09-23 19:36:17 +0900

    Title: 세 정수의 최솟값, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250923/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 250923/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,6 +1,23 @@
 a, b, c = map(int, input().split())
 
-if a < b and a < c:
+if a == b and b == c:
+    print(a)
+elif a == b and a != c:
+    if a < c:
+        print(a)
+    else:
+        print(c)
+elif a == c and a != b:
+    if a < b:
+        print(a)
+    else:
+        print(b)
+elif b == c and b != a:
+    if b < a:
+        print(b)
+    else:
+        print(a)
+elif a < b and a < c:
     print(a)
 elif b < a and b < c:
     print(b)
